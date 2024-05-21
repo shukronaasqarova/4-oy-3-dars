cb5977c9e43cc13ed5c92cda0765c39a2c049cb2
shukronaasqarova <shukronaasqarova94@gmail.com>
2024-05-21 22:50:03 +0500

    end

---
+++ index.js
@@ -0,0 +1,159 @@
+// boolean1
+
+// alert ('a soni musbat jumlani rostlikka tekshiring')
+// let a = +prompt ('a');
+// if (a > 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// } 
+
+// boolean2
+
+// alert ('a soni toq jumlani rostlikka tekshiring')
+// let a = +prompt ('a');
+// if (a % 2 == 1) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// } 
+
+// boolean3
+
+// alert ('a soni juft jumlani rostlikka tekshiring')
+// let a = +prompt ('a');
+// if (a % 2 == 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// } 
+
+// boolean4
+
+// alert ('a va b soni berilgan jumlani rostlikka tekshiring')
+// let a = +prompt ('a');
+// let b  = +prompt ('b');
+// if (a > 2 && b <=3) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// } 
+
+// boolean5
+
+// alert ('a va b soni berilgan jumlani rostlikka tekshiring')
+// let a = +prompt ('a');
+// let b  = +prompt ('b');
+// if (a >= 0 || b <-2) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// } 
+
+// boolean6
+
+// alert ('a b c soni berilgan jumlani rostlikka tekshiring')
+// let a = +prompt ('a');
+// let b  = +prompt ('b');
+// let c  = +prompt ('c');
+// if (a < b && b<=c) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// } 
+
+// boolean7
+
+// alert ('a b c soni berilgan jumlani rostlikka tekshiring')
+// let a = +prompt ('a');
+// let b  = +prompt ('b');
+// let c  = +prompt ('c');
+// if (a <= b && b <= c || a >= c) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// } 
+
+// boolean8
+
+// alert ('a va b toq soni berilgan jumlani rostlikka tekshiring')
+// let a = +prompt ('a');
+// let b  = +prompt ('b');
+// if (a % 2 == 1 && b % 2 == 1) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// } 
+
+// boolean9
+
+// alert ('a va b toq soni berilgan jumlani rostlikka tekshiring a va b sonlarining hech bolmaganda bittasi toq son')
+// let a = +prompt ('a');
+// let b  = +prompt ('b');
+// if (a % 2 == 1 || b % 2 == 1) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// } 
+
+// boolean10
+
+// boolean11
+
+
+// alert ('a va b toq soni berilgan jumlani rostlikka tekshiring a va b sonlarining har ikkalasiham toq son yoki juft son')
+// let a = +prompt ('a');
+// let b  = +prompt ('b');
+// if (a % 2 == 1 && b % 2 == 1 || a % 2 == 0 && b % 2 == 0){
+//     console.log(true);
+// } else {
+//     console.log(false);
+// } 
+
+// boolean12
+
+// alert ('a b c soni berilgan jumlani rostlikka tekshiring sonlarning har biri musbat')
+// let a = +prompt ('a');
+// let b  = +prompt ('b');
+// let c  = +prompt ('c');
+// if (a > 0 && b > 0 && c > 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// } 
+
+// boolean13
+
+// alert ('a b c soni berilgan jumlani rostlikka tekshiring sonlarning hech bolmaganda bittasi musbat')
+// let a = +prompt ('a');
+// let b  = +prompt ('b');
+// let c  = +prompt ('c');
+// if (a > 0 || b > 0 || c > 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// } 
+
+// boolean14
+
+// boolean15
+
+// alert ('a b c soni berilgan jumlani rostlikka tekshiring sonlarning faqat ikkitasi musbat musbat')
+// let a = +prompt ('a');
+// let b  = +prompt ('b');
+// let c  = +prompt ('c');
+// if (a > 0 && b > 0 || b > 0 && c > 0 || a > 0 && c > 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// } 
+
+// boolean16
+
+// alert ('musbat son berilgan jumlani rostlikka tekshiring son ikki xonalik juft son')
+// let a = +prompt ('a');
+// if  (a % 2 == 0 && a > 9 && a < 100) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// } 
